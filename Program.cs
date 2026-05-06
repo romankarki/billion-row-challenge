97fc26bb6f2c558f86b4a477d0234cf04f4558e4
romankarki <romnkrki@gmail.com>
2026-05-05 21:09:49 -0500

    C# code commited

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿var data = File.ReadAllLines("data/weather_stations.csv");
+
+Console.WriteLine(data.Length);
+
+foreach (var line in data)
+{
+    var trimmed = line.TrimStart();
+    if (trimmed.Length == 0 || trimmed[0] == '#')
+        continue;
+
+    var columns = line.Split(';', StringSplitOptions.TrimEntries);
+    if (columns.Length < 2)
+        continue;
+
+    var city = columns[0];
+    var temperature = columns[1];
+    // Console.WriteLine($"{city};{temperature}");
+}
+
